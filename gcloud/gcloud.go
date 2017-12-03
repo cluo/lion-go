2a810629566a2d0f0d4107df244e8828b9f7bd5c
Peter Edge <peter.edge@gmail.com>
2017-12-03 15:37:49 -0500

    Comment out gcloud for now due to import not found

--- gcloud/gcloud.go
+++ gcloud/gcloud.go
@@ -3,20 +3,20 @@
 // See https://cloud.google.com/logging/docs/ for more details.
 package gcloudlion
 
-import (
-	"go.pedge.io/lion"
-	"google.golang.org/api/logging/v1beta3"
-)
+//import (
+//"go.pedge.io/lion"
+//"google.golang.org/api/logging/v1beta3"
+//)
 
-// NewPusher creates a new lion.Pusher that logs using the Google Cloud Logging API.
-func NewPusher(
-	service *logging.ProjectsLogsEntriesService,
-	projectID string,
-	logName string,
-) lion.Pusher {
-	return newPusher(
-		service,
-		projectID,
-		logName,
-	)
-}
+//// NewPusher creates a new lion.Pusher that logs using the Google Cloud Logging API.
+//func NewPusher(
+//service *logging.ProjectsLogsEntriesService,
+//projectID string,
+//logName string,
+//) lion.Pusher {
+//return newPusher(
+//service,
+//projectID,
+//logName,
+//)
+//}
